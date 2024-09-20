dd9a53ffbfec101d7662188d917537a33fdf4c85
rlylo <lylomain@gmail.com>
2024-09-20 16:34:39 -0500

    File created , task and task solution added

---
+++ src/warmup/SumDouble.java
@@ -0,0 +1,27 @@
+package warmup;
+
+public class SumDouble {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public int sumDouble(int a, int b) {
+        if (a == b) {
+            return (a + b) * 2;
+        } else {
+            return a + b;
+        }
+
+    }
+/*
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8*/
+
+
+}
