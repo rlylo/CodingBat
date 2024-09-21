e07bbdbf1510e61f8759a4daab67020cc320619a
rlylo <lylomain@gmail.com>
2024-09-21 13:05:41 -0500

    created file

--- src/warmup/makes10.java
+++ src/warmup/makes10.java
@@ -3,6 +3,7 @@ package warmup;
 public class makes10 {
     public static void main(String[] args) {
 
+
     }
     /*Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
     //test 
