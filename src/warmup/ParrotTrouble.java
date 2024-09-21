29a3cf290fd0515341db68a5fadcbfe33d7d10e7
rlylo <lylomain@gmail.com>
2024-09-21 13:05:08 -0500

    Added task solution and tested code

--- src/warmup/ParrotTrouble.java
+++ src/warmup/ParrotTrouble.java
@@ -2,9 +2,22 @@ package warmup;
 
 public class ParrotTrouble {
     public static void main(String[] args) {
+        boolean parrotNoise = true;
+        int hour = 6;
+        System.out.println(parrotTrouble(parrotNoise,hour));
 
     }
-    /*We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+    public static boolean parrotTrouble(boolean talking,int hour){
+        if(talking && (hour>20 || hour<7)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    /*We have a loud talking parrot.
+    The "hour" parameter is the current hour time in the range 0..23.
+     We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+     Return true if we are in trouble.
 
 
     parrotTrouble(true, 6) → true
