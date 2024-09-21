667ccd8462bd7d07ef9a3a1b05a3893a6afc1390
rlylo <lylomain@gmail.com>
2024-09-21 12:03:02 -0500

    added task description

--- src/warmup/diff21.java
+++ src/warmup/diff21.java
@@ -12,4 +12,10 @@ public class diff21 {
         }
 
     }
+    /*Given an int n, return the absolute difference between n and 21,
+    except return double the absolute difference if n is over 21.
+    test
+    diff21(19) → 2
+    diff21(10) → 11
+    diff21(21) → 0*/
 }
