b8ff731057aff48c99d555110545b1ed4da22daf
rlylo <lylomain@gmail.com>
2024-09-21 12:48:39 -0500

    created file for task makes10 and task description added

---
+++ src/warmup/makes10.java
@@ -0,0 +1,14 @@
+package warmup;
+
+public class makes10 {
+    public static void main(String[] args) {
+
+    }
+    /*Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true*/
+
+}
