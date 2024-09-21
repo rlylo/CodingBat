a4b7edd227e501e875e165016c187d632db89de3
rlylo <lylomain@gmail.com>
2024-09-21 13:09:46 -0500

    added task , task solution and test code

--- src/warmup/makes10.java
+++ src/warmup/makes10.java
@@ -2,9 +2,22 @@ package warmup;
 
 public class makes10 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        System.out.println(makes10(a,b));
+
 
 
     }
+        public static boolean makes10(int a , int b){
+            if (a+b==10 || (a == 10 || b == 10)){
+                return true;
+            }else {
+                return false;
+            }
+
+        }
+
     /*Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
     //test 
     
